e18e45cc35e97ac926908988a69a234acb108532
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-02 14:49:30 +0800

    Week-2 day-4 exercise rainbow boxes finished.

---
+++ week-02/day-4/rainbow-boxes/rainbow-box-function.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+const midX = canvas.clientWidth / 2;
+const midY = canvas.clientHeight / 2;
+const rainbowColors = ['red', 'orange', 'yellow', 'green', 'green', 'blue', 'indigo', 'violet'];
+const gap = 20;
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Create a square drawing function that takes 2 parameters:
+// The square size, and the fill color,
+// and draws a square of that size and color to the center of the canvas.
+// Create a loop that fills the canvas with rainbow colored squares.
+
+function rainBox(size, color) {
+  const x = midX - size / 2;
+  const y = midY - size / 2;
+  ctx.strokeStyle = color;
+  ctx.strokeRect(x, y, size, size);
+}
+
+for (let i = 1; i <= canvas.clientWidth / gap; i++) {
+  rainbowColors.forEach((color, index) => {
+    rainBox(i * 20, color);
+    if (index !== rainbowColors.length - 1) {
+      i++;
+    }
+  });
+}
