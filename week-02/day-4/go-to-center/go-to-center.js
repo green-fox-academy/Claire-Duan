932d84c2293344453883dc758da379c5f813c06c
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-02 13:24:28 +0800

    Week-2 day-4 go-to-center exercise finished.

--- week-02/day-4/go-to-center/go-to-center.js
+++ week-02/day-4/go-to-center/go-to-center.js
@@ -4,9 +4,24 @@ const canvas = document.querySelector('.main-canvas');
 
 const ctx = canvas.getContext('2d');
 
+const height = canvas.clientHeight;
+const width = canvas.clientWidth;
+
 // DO NOT TOUCH THE CODE ABOVE THIS LINE
 
 // Create a line drawing function that takes 2 parameters:
 // The x and y coordinates of the line's starting point
 // and draws a line from that point to the center of the canvas
 // Draw 3 lines with that function. Use loop for that.
+
+function drawLine(x, y) {
+  ctx.beginPath();
+  ctx.strokeStyle = 'green';
+  ctx.moveTo(x, y);
+  ctx.lineTo(width / 2, height / 2);
+  ctx.stroke();
+}
+
+for (let i = 10; i < 40; i += 10) {
+  drawLine(i, i);
+}
