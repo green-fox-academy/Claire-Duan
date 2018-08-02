2814f0ea3112678c231dc47a78f7224805751848
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-02 13:49:28 +0800

    Week-2 day-4 exercise centered sqaure finished.

---
+++ week-02/day-4/square-in-the-center/centered-square.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+const midX = canvas.clientWidth / 2;
+const midY = canvas.clientHeight / 2;
+const width = 10;
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+// Draw a green 10x10 square to the center of the canvas.
+
+ctx.strokeStyle = 'black';
+ctx.strokeRect(midX - width / 2, midY - width / 2, width, width);
