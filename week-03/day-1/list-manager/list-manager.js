4ba96b51d193e0c602657addc45c64038f6e99dd
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-06 17:59:37 +0800

    Week3 day-1 list manager-up and down and move finished.

--- week-03/day-1/list-manager/list-manager.js
+++ week-03/day-1/list-manager/list-manager.js
@@ -4,12 +4,12 @@ const operator = document.getElementsByClassName('operater')[0];
 let left = true;
 
 const leftItems = document.getElementsByClassName('left-items');
-let leftPointer = 0;
+let leftPointer = 4;
 
 const rightItems = document.getElementsByClassName('right-items');
 let rightPointer = 0;
 
-const items = document.getElementsByClassName('items');
+const items = document.querySelectorAll('.items');
 let pointer = 0;
 
 const buttons = document.getElementsByTagName('button');
@@ -23,7 +23,11 @@ window.onload = function () {
   function itemClick(i) {
     items[i].onclick = function () {
       pointer = i;
-      left = true;
+      if(i < 4) {
+        left = true;
+      } else {
+        left = false;
+      }
       console.log(i + 'clicked');
     };
   }
@@ -52,6 +56,8 @@ upButton.onclick = function () {
       const tempString = items[pointer - 1].textContent;
       items[pointer - 1].textContent = items[pointer].textContent;
       items[pointer].textContent = tempString;
+      console.log(pointer);
+
     }
   } else {
     window.alert('Empty!');
@@ -66,6 +72,7 @@ downButton.onclick = function () {
       const tempString = items[pointer + 1].textContent;
       items[pointer + 1].textContent = items[pointer].textContent;
       items[pointer].textContent = tempString;
+      console.log(pointer);
     }
   } else {
     window.alert('Empty!');
@@ -73,5 +80,26 @@ downButton.onclick = function () {
 };
 
 moveButton.onclick = function () {
+  if (leftItems.length !== 0) {
+    if (pointer < 4 && leftPointer > 0) {
+      rightItems[rightPointer].textContent = items[pointer].textContent;
+      leftItems[pointer].textContent = '';
+      rightPointer++;
+      leftPointer--;
+      console.log(leftPointer);
+    }
+  } else {
+    alert('Nothing to move!');
+  }
+};
 
+deleteButton.onclick = function () {
+  if(left) {
+    for(let i = pointer; i < leftPointer-1; i++) {
+      items[i].textContent = items[i+1].textContent;
+    }
+    console.log(leftPointer);
+    items[leftPointer-1].textContent = '';
+    leftPointer--;
+  }
 };
