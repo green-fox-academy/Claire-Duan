9d691bd9be1640fc9f52378540f0f38322400c68
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-06 15:41:17 +0800

    Week3 day-1 greenfox logo finished.

---
+++ week-03/day-1/greenfox-logo/greenfox-logo.js
@@ -0,0 +1,5 @@
+const leftEye = document.getElementById('left_eye');
+const rightEye = document.getElementById('right_eye');
+
+leftEye.setAttribute('fill', 'tomato');
+rightEye.setAttribute('fill', 'tomato');
