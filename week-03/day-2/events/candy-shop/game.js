11f691d676288ca41343d779bb21494d910efcc6
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-07 17:59:50 +0800

    Week-3 day-2 candy shop modified..

--- week-03/day-2/events/candy-shop/game.js
+++ week-03/day-2/events/candy-shop/game.js
@@ -1,1 +1,40 @@
-const
+const candies = document.querySelector('.candies');
+const lollypops = document.querySelector('.lollypops');
+const speed = document.querySelector('.speed');
+
+const createButton = document.querySelector('.create-candies');
+const buyButton = document.querySelector('.buy-lollypops');
+const rainButton = document.querySelector('.candy-machine');
+
+// let str = lollypops.textContent;
+// console.log(str);
+// let a = str.split('');
+// console.log(a.slice(0,10));
+
+
+
+let candyNum = 0;
+let lollyNum = 0;
+
+function setCandy(num) {
+  candies.textContent = num;
+}
+
+function setLolly() {
+  lollypops.textContent.concat(🍭);
+}
+
+createButton.onclick = () => {
+  candyNum++;
+  setCandy(candyNum);
+};
+
+buyButton.onclick = () => {
+  if (candyNum > 100) {
+    candyNum -= 100;
+    setLolly();
+    setCandy(candyNum);
+  } else {
+    window.alert('No enough candies!');
+  }
+};
