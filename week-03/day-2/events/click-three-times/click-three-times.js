4201e08eaf2c200d48eee17029be9e7923ce42e0
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-07 15:59:22 +0800

    Week-3 day-2 click-three-times modified.

--- week-03/day-2/events/click-three-times/click-three-times.js
+++ week-03/day-2/events/click-three-times/click-three-times.js
@@ -1,3 +1,25 @@
 'use strict';
 
-const button = document.querySelector('button');
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+  let counter = 0;
+  let afterFiveSecond = false;
+  const text = '5 seconds ellapsed and clicked 3 times';
+
+  setTimeout(() => { afterFiveSecond = true; console.log(afterFiveSecond); }, 5000);
+  function displayLine() {
+    const newElement = document.createElement('p');
+    newElement.textContent = text;
+    button.parentNode.appendChild(newElement);
+  }
+
+  button.onclick = () => {
+    counter++;
+    if (counter === 3 && afterFiveSecond) {
+      displayLine();
+      counter = 0;
+    } else if (!afterFiveSecond) {
+      counter = 0;
+    }
+  };
+});
