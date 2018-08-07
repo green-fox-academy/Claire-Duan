c4558a156b24c855467dc9adc7fb1be400370c42
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-07 15:43:51 +0800

    Week-3 day-2 delayed-click modified.

--- week-03/day-2/events/delayed-click/delayed-click.js
+++ week-03/day-2/events/delayed-click/delayed-click.js
@@ -3,18 +3,12 @@
 const button = document.querySelector('button');
 const text = '2 seconds ellapsed';
 
-//Method 1
 function displayLine() {
-  console.log(text);
+  const newElement = document.createElement('p');
+  newElement.textContent = text;
+  button.parentNode.appendChild(newElement);
 }
 
 button.onclick = () => {
   setTimeout(displayLine, 2000);
 };
-
-//Method 2
-// function displayLine() {
-//   setTimeout(console.log, 2000, text);
-// }
-
-// button.onclick = displayLine;
