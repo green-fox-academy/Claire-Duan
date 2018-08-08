e19a26ebfd3d531be76eb1b06769da6f0eaa7ff7
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-08 10:46:35 +0800

    Week-3 day-3 string3 finished.

--- week-03/day-3/string1/string1.js
+++ week-03/day-3/string1/string1.js
@@ -1,10 +1,9 @@
-'use strict';
-
 function string1(str) {
-  if (str.length === 1) {
-    return str;
+  if (str.length === 0) {
+    return '';
   }
-  return str.substring(0, 1) + '*' + string1(str.substring(1));
+  return (str.substring(0, 1) === 'x') ? `y${string1(str.substring(1))}`
+    : str.substring(0, 1) + string1(str.substring(1));
 }
 
-console.log(string1('string'));
+console.log(string1('xellowXx'));
