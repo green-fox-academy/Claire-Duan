ac5d61088cf9fe76ee4bc540b16b8d6a2c224382
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-08 15:02:35 +0800

    Week-3 day-2 infinite scroll finished.

--- week-03/day-2/events/infinite-scroll/infinite-scroll.js
+++ week-03/day-2/events/infinite-scroll/infinite-scroll.js
@@ -1,1 +1,35 @@
+'use strict';
+
 const body = document.body;
+const script = document.querySelector('script');
+const scrollThreshold = 300;
+const initialNum = 10;
+
+function randomNum() {
+  return Math.floor(Math.random() * 255);
+}
+
+function generateColor() {
+  return [randomNum(), randomNum(), randomNum()];
+}
+
+function createNewDiv() {
+  const newDiv = document.createElement('div');
+  const colorArr = generateColor();
+  newDiv.style.backgroundColor = `rgb(${colorArr[0]}, ${colorArr[1]}, ${colorArr[2]})`;
+  return newDiv;
+}
+
+function generateDivs() {
+  for (let i = 0; i < initialNum; i++) {
+    body.insertBefore(createNewDiv(), script);
+  }
+}
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY + window.innerHeight >= body.scrollHeight - scrollThreshold) {
+    generateDivs();
+  }
+});
+
+generateDivs();
