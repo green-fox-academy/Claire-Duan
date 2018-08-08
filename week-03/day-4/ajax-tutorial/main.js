78a610b2d87e9f1532532f7b86d43e618ee665df
Claire_Duan <Claire_Duan@epam.com>
2018-08-08 23:17:52 +0800

    Week-3 day-4 AJAX tutorial.

---
+++ week-03/day-4/ajax-tutorial/main.js
@@ -0,0 +1,56 @@
+'use  strict';
+
+const btn = document.getElementById('btn');
+const animalContainer = document.getElementById('animal-info');
+let pageCounter = 1;
+
+btn.addEventListener('click', () => {
+  let ourRequest = new XMLHttpRequest();
+  ourRequest.open('GET', `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`);
+  ourRequest.onload = function () {
+    if (ourRequest.status >= 200 & ourRequest.status < 400) {
+      let ourData =  JSON.parse(ourRequest.responseText);
+      renderHTML(ourData);
+    } else {
+        
+    }
+  };
+
+  ourRequest.onerror = () => {
+
+  };
+
+  ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add('hide-me');
+  }
+});
+
+function renderHTML(data) {
+  let htmlString = '';
+
+  for (let i = 0; i < data.length; i++) {
+    htmlString += `<p>${data[i].name} id a ${data[i].species} that likes to eat `;
+    for (let ii = 0; ii < data[i].foods.likes.length; ii++) {
+      if( ii == 0) {
+      htmlString += data[i].foods.likes[ii];            
+      } else {
+          htmlString += ` and ${data[i].foods.likes[ii]}`;
+      }
+    }
+
+    htmlString += ' and dislikes ';
+
+    for (let ii = 0; ii < data[i].foods.dislikes.length; ii++) {
+        if (ii == 0) {
+        htmlString += data[i].foods.dislikes[ii];            
+        } else {
+            htmlString += ` and ${data[i].foods.dislikes[ii]}`;
+        }
+      }
+
+    htmlString += '.</p>';
+  }
+  animalContainer.insertAdjacentHTML('beforeend', htmlString);
+}
