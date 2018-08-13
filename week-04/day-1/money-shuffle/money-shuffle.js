cf083eee7c4a4370f9771101d356b2f4a25c1edf
CLaire_Duan <CLaire_Duan@epam.com>
2018-08-13 10:49:59 +0800

    Week-4 day-1 money-shuffle finished.

--- week-04/day-1/money-shuffle/money-shuffle.js
+++ week-04/day-1/money-shuffle/money-shuffle.js
@@ -1,35 +1,34 @@
 const Panama = {
-    cash: 0,
-    name: 'Panama',
-    tax: '1%',
-    deposit: function(amount) {
-        this.cash += amount * (1 - parseInt(this.tax) * 0.01);
-    },
-}
+  cash: 0,
+  name: 'Panama',
+  tax: '1%',
+  deposit: function (amount) {
+    this.cash += amount * (1 - parseInt(this.tax, 10) * 0.01);
+  },
+};
 
 const Cyprus = {
-    cash: 500,
-    name: 'Cyprus',
-    tax: '5%',
-    deposit: function(amount) {
-        this.cash += amount * (1 - parseInt(this.tax) * 0.01);        
-    },
-}
+  cash: 500,
+  name: 'Cyprus',
+  tax: '5%',
+  deposit: function (amount) {
+    this.cash += amount * (1 - parseInt(this.tax, 10) * 0.01);
+  },
+};
 
 const Shuffler = {
-    cash: 10000,
-    pick: function(person) {
-        this.cash -= 1000;
-        person.deposit(1000);
-        console.log(`${person.name} got 1000`);
-        
-    }
-}
+  cash: 10000,
+  pick: function (person) {
+    this.cash -= 1000;
+    person.deposit(1000);
+    console.log(`${person.name} got 1000`);
+  },
+};
 
-Shuffler.pick(Panama) // prints Panama got 1000
-Shuffler.pick(Cyprus) // prints Cyprus got 1000
-Shuffler.pick(Panama) // prints Panama got 1000
-Shuffler.pick(Cyprus) // prints Cyprus got 1000
+Shuffler.pick(Panama); // prints Panama got 1000
+Shuffler.pick(Cyprus); // prints Cyprus got 1000
+Shuffler.pick(Panama); // prints Panama got 1000
+Shuffler.pick(Cyprus); // prints Cyprus got 1000
 
-console.log( Panama.cash ) // 2000 
-console.log( Cyprus.cash ) // 2000 
+console.log(Panama.cash); // 2000
+console.log(Cyprus.cash); // 2000
